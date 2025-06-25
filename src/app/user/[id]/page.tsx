7ec6fe62b2b8f28da352713fc5870b5701577f35
Gonzalez Nahuel <nahuelg976@gmail.com>
2025-06-24 21:05:15 -0300

    Quitando el async de la funcion page de user/[id]/page.tsx

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,6 +1,12 @@
 import { UserCard } from "@/components/UserCard";
 
-export default async function Page({ params }: { params: { id: string } }) {
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default function Page({ params }: PageProps) {
   const { id } = params;
 
   return <UserCard id={id} />;
